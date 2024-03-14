91b6c5d14855463c6a6323dcd67f7e9318fdabaa
Parth4120 <patelparth4120@gmail.com>
2024-03-14 16:09:32 +0530

    shopKeeperprofile

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Router, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import UserProfile from './pages/UserProfile';
+import ShopkeeperProfile from './pages/ShopkeeperProfile';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path="/user_profile" element={<UserProfile/>}/>
-
+        <Route path="/shopkeeper_profile" element={<ShopkeeperProfile/>} />
       </Routes>
     
     </div>
