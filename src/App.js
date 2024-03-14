492adce4ba5b9a0c523b92b8fe3d675200c69df6
Parth4120 <patelparth4120@gmail.com>
2024-03-14 21:46:54 +0530

    otppage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Home from './pages/Home';
 import UserProfile from './pages/UserProfile';
 import ShopkeeperProfile from './pages/ShopkeeperProfile';
+import OtpPage from './pages/OtpPage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<Home/>} />
         <Route path="/user_profile" element={<UserProfile/>}/>
         <Route path="/shopkeeper_profile" element={<ShopkeeperProfile/>} />
+        <Route path='/otp' element={<OtpPage/>} />
       </Routes>
     
     </div>
